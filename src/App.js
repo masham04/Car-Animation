a1577b64dd9e2d111d2a3acf2d44fce1e82258b1
masham04 <66207787+masham04@users.noreply.github.com>
2020-08-24 11:10:48 +0500

    first update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import car from './images/car.png';
+import wheel from './images/wheel.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div class="hero">
+	           <div class="highway"></div>
+	           <div class="city"></div>
+	           <div  class="car"><img src={car}></img></div>
+	           <div class="wheel">
+	         	  <img src={wheel} id="wheel1"></img>
+		          <img src={wheel} id="wheel2"></img>
+	             </div>
+      </div>
+   
   );
 }
 
